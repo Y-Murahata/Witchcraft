e21aa7c87ac449dee5d84692ef514b955991d759
TSITCL\s162129 <taka1122judo@gmail.com>
2017-05-26 16:52:56 +0900

    ウェ～ブ管理を作った

--- MoneyWorld/Assets/Scripts/Nagaura/EnemyGenerator.cs
+++ MoneyWorld/Assets/Scripts/Nagaura/EnemyGenerator.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
+    const float SPAN = 1.0f;
+    const float WAVE_SPAN = 15.0f;
+    const int SPAWN_ENEMY_NUM = 10;
 
     public GameObject m_EnemyPrefab;
-    float span = 1.0f;
     private float m_Delta = 0.0f;
-
+    private int m_RemainEnemy = SPAWN_ENEMY_NUM;
+    private bool m_isAssult = true;
     // Use this for initialization
     void Start ()
     {
@@ -21,7 +24,17 @@ public class EnemyGenerator : MonoBehaviour
         // 経過秒数を数える
         m_Delta += Time.deltaTime;
         // 経過秒数が生成スパンより多いか判定
-        if (m_Delta > span)
+        if (!m_isAssult)
+        {
+            if (m_Delta > WAVE_SPAN)
+            {
+                m_Delta = 0.0f;
+                m_isAssult = true;
+                m_RemainEnemy = SPAWN_ENEMY_NUM;
+            }
+            return;
+        }
+        if (m_Delta > SPAN)
         {
             // 経過秒数の初期化
             m_Delta = 0.0f;
@@ -29,6 +42,12 @@ public class EnemyGenerator : MonoBehaviour
             GameObject enemy = Instantiate(m_EnemyPrefab) as GameObject;
             // エネミーの座標の初期化
             enemy.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            // 残りの敵の数を減らす
+            m_RemainEnemy--;
+            if (m_RemainEnemy <= 0)
+            {
+                m_isAssult = false;
+            }
         }
 
     }
